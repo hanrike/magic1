ba3ebbf2b289d65ed2b4c527b69a92336b7d2391
jarol <jaancastano@utp.edu.co>
2022-05-26 16:39:56 -0500

    agregar despliegue de dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import Link from 'next/link';
 const Home: NextPage = () => {
   return (
     <div className='bg-green-500'>
-     <p>Pagina de index</p>
+     <p>Pagina de index -prueba dev</p>
      <Link href='/admin/usuarios'>
        <p className='cursor-pointer'>Ir a admin de usuarios</p>
      </Link>
